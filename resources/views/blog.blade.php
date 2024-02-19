387683e363d9985c21165262a8b5e85fa23eeab5
fikrifaqih80 <fikrifaqiha@gmail.com>
2024-02-19 11:06:42 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ini View Pertama Saya</title>
+</head>
+<body>
+    
+    First View in Laravel
+    Perkenalkan Nama Saya Fikri Faqih Al Fawwaz
+</body>
+</html>
